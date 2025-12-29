c48f578060df9cea7afc8203697d7b5380b79642
xandertang <xandertang@qq.com>
2025-12-29 11:35:19 +0800

    Comment out buttons and links in AppHeader

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -48,7 +48,7 @@ export default function AppHeader({ onOpenSettings }) {
         
         {/* Right Actions */}
         <div className="flex items-center gap-3 sm:gap-4 ml-auto">
-          {/* Notice Button */}
+          {/* Notice Button
           <button
             type="button"
             onClick={handleNoticeClick}
@@ -56,7 +56,8 @@ export default function AppHeader({ onOpenSettings }) {
           >
             🎁 获取访问密码
           </button>
-          {/* Buy Me a Coffee Button */}
+           */}
+          {/* Buy Me a Coffee Button
           <button
             type="button"
             onClick={() => setIsSponsorOpen(true)}
@@ -83,6 +84,7 @@ export default function AppHeader({ onOpenSettings }) {
             </svg>
             <span>请作者喝杯奶茶</span>
           </button>
+           */}
           {/* Settings Button */}
           <button
             type="button"
@@ -93,7 +95,7 @@ export default function AppHeader({ onOpenSettings }) {
           >
             <Settings className="w-[22px] h-[22px]" />
           </button>
-          {/* Documentation Link */}
+          {/* Documentation Link
           <a
             href="https://smart-draw-doc.aizhi.site"
             target="_blank"
@@ -117,7 +119,8 @@ export default function AppHeader({ onOpenSettings }) {
               <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
             </svg>
           </a>
-          {/* GitHub Link */}
+           */}
+          {/* GitHub Link
           <a
             href="https://github.com/liujuntao123/smart-draw"
             target="_blank"
@@ -135,7 +138,7 @@ export default function AppHeader({ onOpenSettings }) {
               <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
             </svg>
           </a>
-
+ */}
         </div>
       </header>
 
